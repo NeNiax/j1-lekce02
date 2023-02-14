24caa32b680a0f9aedd39dd71aec51633761e607
Neniax <peti69501@gmail.com>
2023-02-14 21:02:51 +0100

    Domeček

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -8,6 +8,40 @@ public class HlavniProgram {
         zofka = new Turtle();
 
         //TODO tady bude kód
+
+        // toto je řádek, který domeček postaví
+        zofka.turnRight(90);
+
+        // udělá čtvereček
+        zofka.turnRight(180);
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+        //udělá střechu
+        zofka.turnRight(45);
+        zofka.move(70);
+        zofka.turnLeft(90);
+        zofka.move(70);
+
+        // tady se přidají nožičky
+/*
+        nozicka();
+        zofka.turnRight(30);
+        zofka.move(100);
+        zofka.turnRight(30);
+        nozicka();
+*/
+    }
+
+    private void nozicka() {
+        zofka.move(25);
+        zofka.turnRight(180);
+        zofka.move(25);
+        zofka.turnLeft(75);
+        zofka.move(25);
+        zofka.turnLeft(180);
+        zofka.move(25);
     }
 
     public static void main(String[] args) {
